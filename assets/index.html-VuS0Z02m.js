493a8f448020a404ad706e0151dc7c9d0a6ce993
samwafgo <138433335+samwafgo@users.noreply.github.com>
2024-10-28 08:35:45 +0000

    Deploying to gh-pages from @ samwafgo/samwaf.github.io@52448f93e9d0eb82bbbe7cce3754d7f5ef5811a7 🚀

---
+++ assets/index.html-VuS0Z02m.js
@@ -0,0 +1,25 @@
+import{_ as l}from"./overview-bkHU70fV.js";import{_ as t}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as s,o as r,c,a as e,d as a,b as i,e as o}from"./app-XH9EmEgC.js";const u={},m=e("h1",{id:"快速上手",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#快速上手","aria-hidden":"true"},"#"),a(" 快速上手")],-1),v=e("div",{class:"hint-container warning"},[e("p",{class:"hint-container-title"},"注意"),e("p",null,[e("strong",null,"强烈建议您在测试环境测试充分在上生产，如遇到问题请及时反馈")])],-1),h={id:"_1-下载最新版本",tabindex:"-1"},b=e("a",{class:"header-anchor",href:"#_1-下载最新版本","aria-hidden":"true"},"#",-1),x={href:"https://gitee.com/samwaf/SamWaf/releases",target:"_blank",rel:"noopener noreferrer"},f={href:"https://github.com/samwafgo/SamWaf/releases",target:"_blank",rel:"noopener noreferrer"},p=o(`<h2 id="_2-快速启动" tabindex="-1"><a class="header-anchor" href="#_2-快速启动" aria-hidden="true">#</a> 2.快速启动</h2><h3 id="windows" tabindex="-1"><a class="header-anchor" href="#windows" aria-hidden="true">#</a> Windows</h3><ul><li>直接启动</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>SamWaf64.exe
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li>服务形式</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>//安装
+SamWaf64.exe install 
+
+//启动
+SamWaf64.exe start
+
+//停止
+SamWaf64.exe stop
+
+//卸载
+SamWaf64.exe uninstall
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="linux" tabindex="-1"><a class="header-anchor" href="#linux" aria-hidden="true">#</a> Linux</h3><ul><li>直接启动</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>./SamWafLinux64
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li>服务形式</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>//安装
+./SamWafLinux64 install 
+
+//启动
+./SamWafLinux64 start
+
+//停止
+./SamWafLinux64 stop
+
+//卸载
+./SamWafLinux64 uninstall
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="hint-container warning"><p class="hint-container-title">注意</p><p>PS: 注意从v1.3.7-beta.1 开始如果是windows server 2008r2 的系统请下载 SamWaf64ForWin7Win8Win2008.exe 使用。</p></div><h2 id="_3-启动访问" tabindex="-1"><a class="header-anchor" href="#_3-启动访问" aria-hidden="true">#</a> 3.启动访问</h2><p>http://127.0.0.1:26666</p><p>默认帐号：admin 默认密码：admin868 (注意首次进入请把默认密码改掉)</p><figure><img src="`+l+'" alt="SamWaf主画面" tabindex="0" loading="lazy"><figcaption>SamWaf主画面</figcaption></figure>',16);function _(g,W){const d=s("Badge"),n=s("ExternalLinkIcon");return r(),c("div",null,[m,v,e("h2",h,[b,a(" 1.下载最新版本 "),i(d,{text:"v1.3.6",type:"tip"})]),e("p",null,[a("gitee: "),e("a",x,[a("https://gitee.com/samwaf/SamWaf/releases"),i(n)])]),e("p",null,[a("github: "),e("a",f,[a("https://github.com/samwafgo/SamWaf/releases"),i(n)])]),p])}const k=t(u,[["render",_],["__file","index.html.vue"]]);export{k as default};
