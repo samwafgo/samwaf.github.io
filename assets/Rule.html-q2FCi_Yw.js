06df4d5114dad2ea74c9c739a6fbc43a89da3801
samwafgo <138433335+samwafgo@users.noreply.github.com>
2025-02-10 01:49:27 +0000

    Deploying to gh-pages from @ samwafgo/samwaf.github.io@cd5b4c5ce8726aadc50e92f9921540b063292882 🚀

---
+++ assets/Rule.html-q2FCi_Yw.js
@@ -0,0 +1,36 @@
+import{_ as e,a as i,b as n,c as a}from"./manual_rule-brzwEAqZ.js";import{_ as r}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as d,e as l}from"./app-x7tOvUR4.js";const t={},c=l('<h1 id="protection-rules" tabindex="-1"><a class="header-anchor" href="#protection-rules" aria-hidden="true">#</a> Protection Rules</h1><h2 id="_1-script-editing" tabindex="-1"><a class="header-anchor" href="#_1-script-editing" aria-hidden="true">#</a> 1 Script Editing</h2><h3 id="_1-1-quick-handling-from-logs" tabindex="-1"><a class="header-anchor" href="#_1-1-quick-handling-from-logs" aria-hidden="true">#</a> 1.1 Quick Handling from Logs</h3><p><img src="'+e+'" alt="Log Details" loading="lazy"> Select the desired feature value, turn on the &quot;Quick Add Rule&quot; switch, and click on a blank space.</p><h3 id="_1-2-selecting-feature-values" tabindex="-1"><a class="header-anchor" href="#_1-2-selecting-feature-values" aria-hidden="true">#</a> 1.2 Selecting Feature Values</h3><figure><img src="'+i+'" alt="Selecting Feature Values" tabindex="0" loading="lazy"><figcaption>Selecting Feature Values</figcaption></figure><h3 id="_1-3-automatically-create-rule-script" tabindex="-1"><a class="header-anchor" href="#_1-3-automatically-create-rule-script" aria-hidden="true">#</a> 1.3 Automatically Create Rule Script</h3><figure><img src="'+n+`" alt="Automatically Create Rule Script" tabindex="0" loading="lazy"><figcaption>Automatically Create Rule Script</figcaption></figure><p>Currently, the following data can be identified:</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>Host
+HOST
+
+URL
+URL
+
+Referrer
+REFERER
+
+User-Agent
+USER_AGENT
+
+Request Method
+METHOD
+
+Access COOKIES
+COOKIES
+
+Access BODY
+BODY
+
+Request Port
+PORT
+
+Visitor IP
+SRC_IP
+
+Visitor Country
+COUNTRY
+
+Visitor Province
+PROVINCE
+
+Visitor City
+CITY
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_2-interface-editing" tabindex="-1"><a class="header-anchor" href="#_2-interface-editing" aria-hidden="true">#</a> 2 Interface Editing</h2><p>The current ease of use is not very convenient, it is recommended to use the script method. <img src="`+a+'" alt="Interface Editing" loading="lazy"></p><h2 id="_3-deep-learning-engine" tabindex="-1"><a class="header-anchor" href="#_3-deep-learning-engine" aria-hidden="true">#</a> 3 Deep Learning Engine</h2><p>To be developed.</p>',14),u=[c];function o(v,m){return s(),d("div",null,u)}const p=r(t,[["render",o],["__file","Rule.html.vue"]]);export{p as default};
