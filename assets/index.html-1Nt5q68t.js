541ed6713b203a3a81dfde7a7b4141ddbe0911e0
samwafgo <138433335+samwafgo@users.noreply.github.com>
2025-02-21 06:25:26 +0000

    Deploying to gh-pages from @ samwafgo/samwaf.github.io@a5188a362a22926ab24516bb5d984d843ebf7625 🚀

---
+++ assets/index.html-1Nt5q68t.js
@@ -0,0 +1,25 @@
+import{_ as r}from"./overview-bkHU70fV.js";import{_ as l}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as s,o as t,c,a as e,d as a,b as i,e as o}from"./app-ZDGdy98S.js";const u={},m=e("h1",{id:"快速上手",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#快速上手","aria-hidden":"true"},"#"),a(" 快速上手")],-1),h=e("div",{class:"hint-container warning"},[e("p",{class:"hint-container-title"},"注意"),e("p",null,[e("strong",null,"强烈建议您在测试环境测试充分在上生产，如遇到问题请及时反馈")])],-1),v={id:"_1-下载最新版本",tabindex:"-1"},b=e("a",{class:"header-anchor",href:"#_1-下载最新版本","aria-hidden":"true"},"#",-1),x={href:"https://gitee.com/samwaf/SamWaf/releases",target:"_blank",rel:"noopener noreferrer"},f={href:"https://github.com/samwafgo/SamWaf/releases",target:"_blank",rel:"noopener noreferrer"},p=o(`<h2 id="_2-快速启动" tabindex="-1"><a class="header-anchor" href="#_2-快速启动" aria-hidden="true">#</a> 2.快速启动</h2><h3 id="windows" tabindex="-1"><a class="header-anchor" href="#windows" aria-hidden="true">#</a> Windows</h3><ul><li>直接启动</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>SamWaf64.exe
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li>服务形式</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>//安装
+SamWaf64.exe install 
+
+//启动
+SamWaf64.exe start
+
+//停止
+SamWaf64.exe stop
+
+//卸载
+SamWaf64.exe uninstall
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="linux" tabindex="-1"><a class="header-anchor" href="#linux" aria-hidden="true">#</a> Linux</h3><ul><li>直接启动</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>./SamWafLinux64
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li>服务形式</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>//安装
+./SamWafLinux64 install 
+
+//启动
+./SamWafLinux64 start
+
+//停止
+./SamWafLinux64 stop
+
+//卸载
+./SamWafLinux64 uninstall
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="hint-container warning"><p class="hint-container-title">注意</p><p>PS: 注意从v1.3.7-beta.1 开始如果是windows server 2008r2 的系统请下载 SamWaf64ForWin7Win8Win2008.exe 使用。</p></div><h3 id="linux-arm64架构请下载对应的文件" tabindex="-1"><a class="header-anchor" href="#linux-arm64架构请下载对应的文件" aria-hidden="true">#</a> Linux Arm64架构请下载对应的文件</h3><h3 id="docker-请到dockerhub拉取最新版本" tabindex="-1"><a class="header-anchor" href="#docker-请到dockerhub拉取最新版本" aria-hidden="true">#</a> Docker 请到dockerhub拉取最新版本</h3><h2 id="_3-启动访问" tabindex="-1"><a class="header-anchor" href="#_3-启动访问" aria-hidden="true">#</a> 3.启动访问</h2><p>http://127.0.0.1:26666</p><p>默认帐号：admin 默认密码：admin868 (注意首次进入请把默认密码改掉)</p><figure><img src="`+r+'" alt="SamWaf主画面" tabindex="0" loading="lazy"><figcaption>SamWaf主画面</figcaption></figure>',18);function _(g,W){const d=s("Badge"),n=s("ExternalLinkIcon");return t(),c("div",null,[m,h,e("h2",v,[b,a(" 1.下载最新版本 "),i(d,{text:"v1.3.9",type:"tip"})]),e("p",null,[a("gitee: "),e("a",x,[a("https://gitee.com/samwaf/SamWaf/releases"),i(n)])]),e("p",null,[a("github: "),e("a",f,[a("https://github.com/samwafgo/SamWaf/releases"),i(n)])]),p])}const L=l(u,[["render",_],["__file","index.html.vue"]]);export{L as default};
